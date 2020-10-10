7dfeaee1a43bd8f121b6c1abfffe1f6e7920bef2
Faiq <faiq.019@gmail.com>
2020-10-10 15:16:30 +0500

    Python

---
+++ Python/Exceptions/exceptions.py
@@ -0,0 +1,9 @@
+for i in range(int(input())):
+    try:
+        a,b = map(int,input().split()) 
+        result = a // b
+        print(result)
+    except ZeroDivisionError as x:
+        print("Error Code:", x)
+    except ValueError as y:
+        print("Error Code:", y)
