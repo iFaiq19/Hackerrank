13b110dc1a15ff27a93e7c62c288db79413045e7
Faiq <faiq.019@gmail.com>
2020-10-10 15:53:27 +0500

    Python

--- Python/Any or All/any-all.py
+++ Python/Any or All/any-all.py
@@ -1,2 +1,2 @@
 N, lst = int(input()), list(map(int, input().split()))
-print(all(i > 0 for i in lst) and any(i < 10 or i % 11 == 0 for i in lst))
+print(all(i > 0 for i in lst) and any(i <10 or str(i) == (str(i))[::-1] for i in lst))
