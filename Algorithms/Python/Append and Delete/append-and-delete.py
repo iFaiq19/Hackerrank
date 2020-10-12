e44da962d273c0a124d2e83b3fa83a980fea6236
Faiq <faiq.019@gmail.com>
2020-10-12 23:07:59 +0500

    Append and Delete

--- Algorithms/Python/Append and Delete/append-and-delete.py
+++ Algorithms/Python/Append and Delete/append-and-delete.py
@@ -8,19 +8,21 @@ import sys
 
 # Complete the appendAndDelete function below.
 def appendAndDelete(s, t, k):
+    answer = ' '
     if len(s)+len(t)<k:
-        return 'Yes'
+        answer='Yes'
+    else:
+        sameChar = 0
+        for i,j in zip(s, t):
+            if i == j:
+                sameChar += 1
+            else: break
+        diff = (len(s)-sameChar)+(len(t)-sameChar)
+        if diff <= k and (diff-k)%2 == 0:
+            answer='Yes'
+        else: answer='No'
+    return answer
         
-    sameChar = 0
-    for i,j in zip(s, t):
-        if i == j:
-            sameChar += 1
-        else: break
-    diff = (len(s)-sameChar)+(len(t)-sameChar)
-    if diff <= k and (diff-k)%2 == 0:
-        return 'Yes'
-    else: return 'No'
-    
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
